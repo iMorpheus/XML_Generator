f960cb56153ff89abb32c7cf0c25e94028540ed5
Derek <iMorpheus@users.noreply.github.com>
2019-02-04 13:30:16 +0900

    Update DLABO.sh
    Removed semi colons.

--- DLABO.sh
+++ DLABO.sh
@@ -11,11 +11,11 @@ function DLABO()
 echo '~~~~~~~~~~~~~~~~~~~Audio File Data~~~~~~~~~~~~~~';
 
 ls *.m*;
-read -p "Length in bytes for file: " -e TM4A ;
-M4A=$(echo $TM4A | tr -d "[:space:]");
+read -p "Length in bytes for file: " -e TM4A 
+M4A=$(echo $TM4A | tr -d "[:space:]")
 
-BYTES=$(afinfo $M4A | grep "audio bytes" | grep -Eo '[0-9]{1,15}');
-DUR=$(afinfo $M4A | grep "estimated duration" | grep -Eo '[0-9].{1,10}');
+BYTES=$(afinfo $M4A | grep "audio bytes" | grep -Eo '[0-9]{1,15}')
+DUR=$(afinfo $M4A | grep "estimated duration" | grep -Eo '[0-9].{1,10}')
 
 #### DROPBOX BIT THAT HAS ELUDED ME FOR MONTHS.
 #### AUTOMATION IS HERE!
@@ -36,15 +36,15 @@ TEMPMFL=$(curl -X POST https://api.dropboxapi.com/2/sharing/get_shared_links \
   --data '{"path":"/~'$Names'/'$M4A'"}' | jq -r .links[].url);
   
 # file type audio/mp4a-latm auto added at generation.
-MFL=$(echo "$TEMPMFL" | sed s/https.*com// | sed s/?.*//);
+MFL=$(echo "$TEMPMFL" | sed s/https.*com// | sed s/?.*//)
 
 # Episode Information:
 echo '~~~~~~~~~~~~~~~~~~~Episode Information~~~~~~~~~~~~~~';
-EPSNO=$(afinfo $M4A | grep "File:" | grep -Eo '[0-9][0-9][0-9]');
+EPSNO=$(afinfo $M4A | grep "File:" | grep -Eo '[0-9][0-9][0-9]')
 
-read -p "Episode name: " -e EPSNM;
-read -p "Episode subtitle: " -e EPSSBT;
-read -p "Episode Summary: " -e EPSSUM;
+read -p "Episode name: " -e EPSNM
+read -p "Episode subtitle: " -e EPSSBT
+read -p "Episode Summary: " -e EPSSUM
 
 PS3="Explicit tag. Select from Yes(Explict), No(Not explict) or Clean: "
 select XPLCT in Clean Yes No
@@ -54,14 +54,14 @@ break
 done
 
 # Keywords
-read -p "Add keywords: " -e KYWRDS;
+read -p "Add keywords: " -e KYWRDS
 
 # echo '~~~~~~~~~~~~~~~~~~~Date and Time~~~~~~~~~~~~~~';
-DOW=$(date +"%a");
-CDATE=$(date +"%d");
-MNTH=$(date +"%b");
-PYR=$(date +"%Y");
-TME=$(date +"%H:%M:%S");
+DOW=$(date +"%a")
+CDATE=$(date +"%d")
+MNTH=$(date +"%b")
+PYR=$(date +"%Y")
+TME=$(date +"%H:%M:%S")
 
 
 echo "~~~~~~~~~~~~~~~~~~~~RSS FEED~~~~~~~~~~~~~~~";
@@ -69,13 +69,13 @@ echo "~~~~~~~~~~~~~~~~~~~~RSS FEED~~~~~~~~~~~~~~~";
 # ----------------- END DATA COLLECTION -------------------
 
 # Math
-EstDur=$(echo "($DUR+0.5)/1" | bc);
+EstDur=$(echo "($DUR+0.5)/1" | bc)
 
-hours=$(echo "($EstDur/3600)" | bc);
-minutes=$(echo "(($EstDur/60)%60)" | bc);
-seconds=$(echo "($EstDur%60)" | bc);
-# echo "00:$minutes:$seconds";
-DRTN=$(printf "%02d:%02d:%02d\n" $hours $minutes $seconds);
+hours=$(echo "($EstDur/3600)" | bc)
+minutes=$(echo "(($EstDur/60)%60)" | bc)
+seconds=$(echo "($EstDur%60)" | bc)
+# echo "00:$minutes:$seconds"
+DRTN=$(printf "%02d:%02d:%02d\n" $hours $minutes $seconds)
 
 # ------------------ XML GENERATOR PROPER -------------------
 ex "$RSS" << EOF
@@ -105,7 +105,7 @@ cat << EOF >> "$RSS"
 
 EOF
 
-clear;
+clear
 }
 
 trap DLABO EXIT
